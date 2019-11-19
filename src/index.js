426311d79b294b6dd4b60b2a518e6b2d5039d52a
a.efimova <asi4ka.84@gmail.com>
2019-11-19 09:40:51 +0300

    homework done

---
+++ src/index.js
@@ -0,0 +1,121 @@
+/* ДЗ 2 - работа с массивами и объектами */
+
+/*
+ Задание 1:
+
+ Напишите аналог встроенного метода forEach для работы с массивами
+ Посмотрите как работает forEach и повторите это поведение для массива, который будет передан в параметре array
+ */
+function forEach(array, fn) {
+    for (let i = 0; i < array.length; i++) {
+        fn(array[i], i, array);
+    }
+}
+
+/*
+ Задание 2:
+
+ Напишите аналог встроенного метода map для работы с массивами
+ Посмотрите как работает map и повторите это поведение для массива, который будет передан в параметре array
+ */
+function map(array, fn) {
+    var newArr = [];
+
+    for (let i = 0; i < array.length; i++) {
+        newArr.push(fn(array[i], i, array));
+    }
+
+    return newArr;
+}
+
+/*
+ Задание 3:
+
+ Напишите аналог встроенного метода reduce для работы с массивами
+ Посмотрите как работает reduce и повторите это поведение для массива, который будет передан в параметре array
+ */
+function reduce(array, fn, initial) {
+    var result,
+        from;
+
+    if (initial===undefined) {
+        from = 1;
+        result = array[0];
+    } else {
+        from = 0;
+        result = initial;
+    }
+
+    for (let i = from; i < array.length; i++) {
+        result = fn(result, array[i], i, array);
+    }
+
+    return result;
+}
+
+/*
+ Задание 4:
+
+ Функция должна перебрать все свойства объекта, преобразовать их имена в верхний регистр и вернуть в виде массива
+
+ Пример:
+   upperProps({ name: 'Сергей', lastName: 'Петров' }) вернет ['NAME', 'LASTNAME']
+ */
+function upperProps(obj) {
+    var keys = Object.keys(obj),
+        toUpper = x => x.toUpperCase();
+
+    return keys.map(toUpper);
+}
+
+/*
+ Задание 5 *:
+
+ Напишите аналог встроенного метода slice для работы с массивами
+ Посмотрите как работает slice и повторите это поведение для массива, который будет передан в параметре array
+ */
+function slice(array, from=0, to = array.length) {
+    var newArray = [],
+        prepare = (p) => {
+            if (p>(array.length)) {
+                p = array.length;
+            }
+            if (p<0) {
+                p = array.length+p;
+                if (p<0) {
+                    p=0;
+                }
+            }
+
+            return p;
+        };
+
+    for (let i = prepare(from); i < prepare(to); i++) {
+        newArray.push(array[i]);
+    }
+
+    return newArray;
+}
+
+/*
+ Задание 6 *:
+
+ Функция принимает объект и должна вернуть Proxy для этого объекта
+ Proxy должен перехватывать все попытки записи значений свойств и возводить это значение в квадрат
+ */
+function createProxy(obj) {
+    return new Proxy(obj, {
+        set(obj, key, value) {
+            obj[key] = Math.pow(value, 2);
+        }
+    });
+}
+
+export {
+    forEach,
+    map,
+    reduce,
+    upperProps,
+    slice,
+    createProxy
+};
