5f7d92b2f9c6b77389be583abbb45a259aaee55a
a.efimova <asi4ka.84@gmail.com>
2019-12-23 21:50:15 +0300

    chat: выход пользователя, добавление картинки

--- server/socket.js
+++ server/socket.js
@@ -2,18 +2,17 @@ const WebSocket = require('ws');
 const uuidv1 = require('uuid/v1');
 
 const wss = new WebSocket.Server({ port: 3000 });
+var allUsers = [];
 
 console.log('сервер запущен');
 
 const handlers = {
     'newUser': (data, ws) => {
-        const allUsers = [];
-
-        wss.clients.forEach((client) => {
-            if (client.user) {
-                allUsers.push(client.user);
-            }
-        });
+        // wss.clients.forEach((client) => {
+        //     if (client.user) {
+        //         allUsers.push(client.user);
+        //     }
+        // });
 
         ws.send(JSON.stringify({
             payload: 'getUsers',
@@ -27,11 +26,34 @@ const handlers = {
 
         ws.user = user;
         console.log('добавлен новый пользователь', user);
+        allUsers.push(user);
         broadcast({
             payload: 'newUser',
             data: user
         });
     },
+    'leaveUser': (data, ws) => {
+        console.log('выход пользователя', allUsers);
+        allUsers = allUsers.filter(x => x.id != data.user.id);
+        broadcast({
+            payload: 'leaveUser',
+            data: data.user.id
+        });
+    },
+    'addPhoto': (data, ws) => {
+        console.log('пришла картинка');
+        allUsers.forEach((user) => {
+            if (user.id === data.user.id) {
+                user.img = data.user.img;
+            }
+        });
+
+        console.log(allUsers);
+        broadcast({
+            payload: 'addPhoto',
+            data: data.user
+        });
+    }
 };
 
 function broadcast(message) {
@@ -47,10 +69,6 @@ wss.on('connection', function connection(ws) {
     console.log('установлено соединение');
     ws.on('close', (e) => {
         console.log('пользователь вышел', ws.user);
-        broadcast({
-            payload: 'leaveUser',
-            data: ws.user
-        })
     });
 
     ws.on('message', (data) => {
--- src/index.js
+++ src/index.js
@@ -1,15 +1,42 @@
 var root = document.querySelector('#pageWrap');
+// Объект router выполняет обработчик при добавлении блока с соответствующим id
 var router = {
     'auth': (target) => {
-        const signIn = target.querySelector('button');
+        const signIn = target.querySelector('button'),
+            cancel = target.querySelector('.cancel'),
+            authForm = target.querySelector('form');
+
         signIn.addEventListener('click', () => {
-            var login = target.querySelector('#name').value || 'anonim';
+            var login = target.querySelector('#nik').value || target.querySelector('#name').value;
 
             root.innerHTML = render('chat', { name: login });
+        });
+        cancel.addEventListener('click', () => {
+            authForm.reset();
         });
     },
     'chat': (target) => {
         initSockets(target);
+        // const fileInp = target.querySelector('#fileInp'),
+        //     fileReader = new FileReader();
+        // let currentImg;
+        //
+        // fileInp.addEventListener('change', e => {
+        //     let file = e.target.files[0];
+        //
+        //     fileReader.readAsDataURL(file);
+        //
+        //     console.log(1);
+        // });
+        // target.addEventListener('click', e => {
+        //     if (e.target.classList.contains('img')) {
+        //         fileInp.click();
+        //         currentImg = e.target;
+        //     }
+        // });
+        // fileReader.addEventListener('load', function () {
+        //     currentImg.src = fileReader.result;
+        // });
     }
 };
 // Конфигурация observer (за какими изменениями наблюдать)
@@ -22,13 +49,16 @@ const config = {
 // Функция обратного вызова при срабатывании мутации
 const callback = function (mutationsList, observer) {
     var node = mutationsList[0].addedNodes[0];
-    var route = node.id;
-    console.log(route,node);
-    if (route) {
-        if (router[route]) {
-            router[route](node);
-        } else {
-            console.error('отсутствует обработчик для блока ' + route);
+
+    if (node) {
+        var route = node.id;
+
+        if (route) {
+            if (router[route]) {
+                router[route](node);
+            } else {
+                console.error('отсутствует обработчик для блока ' + route);
+            }
         }
     }
 };
@@ -43,29 +73,45 @@ window.addEventListener('DOMContentLoaded', () => {
 });
 
 function initSockets(target) {
-    var list = target.querySelector('.contactList');
+    var list = target.querySelector('.contactList'),
+        currentUserId = target.dataset.id;
     const ws = new WebSocket('ws://localhost:3000');
 
+    // Массив обработчиков событий пришедших с сервера
     const handlers = {
         'newUser': (data) => {
             console.log('newUser', data);
-            list.innerHTML += `<li data-id="${data.id}">${data.name}</li>`;
+            if (!currentUserId) {
+                target.setAttribute('data-id', data.id);
+                currentUserId = target.dataset.id;
+                data.class = 'myImg';
+            }
+            list.innerHTML += render('user', data);
         },
         'getUsers': (data) => {
             console.log('getUsers', data);
             data.forEach(item => {
-                // console.log(item);
-                list.innerHTML += `<li data-id="${item.id}">${item.name}</li>`;
+                list.innerHTML += render('user', item);
             });
         },
         'leaveUser': (data) => {
             console.log('leaveUser', data);
+            let currentUser = target.querySelector('[data-user="'+data+'"]');
+
+            currentUser.parentElement.removeChild(currentUser);
         },
+        'addPhoto': (data) => {
+            console.log('addPhoto', data);
+            let currentUser = target.querySelector('[data-user="'+data.id+'"]'),
+                currentImg = currentUser.querySelector('.img');
+
+            currentImg.src = data.img;
+        }
     };
 
     console.log(ws);
     ws.onopen = () => {
-        console.log('Клиент подключен');
+        console.log(target, 'Клиент подключен', target.dataset.user);
         ws.send(JSON.stringify({
             payload: 'newUser',
             data: {
@@ -76,6 +122,7 @@ function initSockets(target) {
         }));
     };
 
+    // Событие, которое вызывается при получении любых данных с сервера. Выполняется обработчик из массива handlers
     ws.onmessage = function (e) {
         var dataParse = JSON.parse(e.data);
 
@@ -89,11 +136,45 @@ function initSockets(target) {
     var logOut = target.querySelector('.logOut');
 
     logOut.addEventListener('click', () => {
+        ws.send(JSON.stringify({
+            payload: 'leaveUser',
+            data: {
+                user: {
+                    id: currentUserId
+                }
+            }
+        }));
         ws.close();
         root.innerHTML = render('auth');
+    });
+
+    const fileInp = target.querySelector('#fileInp'),
+        fileReader = new FileReader();
+
+    fileInp.addEventListener('change', e => {
+        let file = e.target.files[0];
+
+        fileReader.readAsDataURL(file);
+    });
+    target.addEventListener('click', e => {
+        if (e.target.classList.contains('myImg')) {
+            fileInp.click();
+        }
+    });
+    fileReader.addEventListener('load', function () {
+        ws.send(JSON.stringify({
+            payload: 'addPhoto',
+            data: {
+                user: {
+                    id: currentUserId,
+                    img: fileReader.result
+                }
+            }
+        }));
     });
 }
 
 function render(templateName, data = '') {
+    console.log(data);
     return require(`./views/${templateName}.hbs`)(data);
 }
